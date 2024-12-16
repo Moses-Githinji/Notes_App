1d66e2fe1a6e057f710152fb430e77c160676453
the-ACE <94229616+Moses-Githinji@users.noreply.github.com>
2024-12-16 03:34:23 +0300

    Finished the build. Now the Notes_App is dynamic, navigable and interactive.

--- Notes/Models/About.cs
+++ Notes/Models/About.cs
@@ -4,6 +4,6 @@ internal class About
 {
     public string Title => AppInfo.Name;
     public string Version => AppInfo.VersionString;
-    public string MoreInfoUrl => "https://aka.ms/maui";
-    public string Message => "This app is written in XAML and C# with .NET MAUI.";
+    public string MoreInfoUrl => "https://github.com/Moses-Githinji";
+    public string Message => "This app is written in XAML and C# with .NET MAUI. The developer is called Githinji";
 }
