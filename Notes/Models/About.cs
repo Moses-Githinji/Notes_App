31c0cf02e23926f28f45b7fad92f367b918d655c
the-ACE <94229616+Moses-Githinji@users.noreply.github.com>
2024-12-16 16:23:32 +0300

    Changed from the old View-Model to the MVVM Pattern - Updated the Note.cs model to the MVVM Pattern that is recommended

--- Notes/Models/About.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Notes.Models;
-
-internal class About
-{
-    public string Title => AppInfo.Name;
-    public string Version => AppInfo.VersionString;
-    public string MoreInfoUrl => "https://github.com/Moses-Githinji";
-    public string Message => "This app is written in XAML and C# with .NET MAUI. The developer is called Githinji";
-}
